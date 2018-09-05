b3dc221f67ab151723568d4735dfa3f4514564d0
VladLaz27 <39669227+VladLaz27@users.noreply.github.com>
2018-09-05 16:33:28 +0300

    Головна сторінка

--- js/index.js
+++ js/index.js
@@ -4,3 +4,20 @@
         prevEl: ".swiper-button-prev",
       },
     });
+
+
+
+    (function($) {
+$(function() {
+
+  $('ul.tabs__caption').each(function() {
+    $(this).find('li').each(function(i) {
+      $(this).click(function(){
+        $(this).addClass('active').siblings().removeClass('active')
+          .closest('div.tabs').find('div.tabs__content').removeClass('active').eq(i).addClass('active');
+      });
+    });
+  });
+
+})
+})(jQuery)
